39b8bb13fa4453f21d205e61737ee98381293d70
RomanSibakovskij <j4023958@gmail.com>
2025-04-02 07:29:34 +0300

    Add relevant web elements for SignUpFormPageInvalidScenarios

--- pages/utils/SignUpFormPageInvalidScenarios.js
+++ pages/utils/SignUpFormPageInvalidScenarios.js
@@ -12,7 +12,14 @@ class SignUpFormPageInvalidScenarios extends BasePage{
     constructor(driver) {
         super(driver);
 
-        
+        //input form
+        this._signUpFormPageFirstNameInputField = By.xpath("//input[@id='s-name']");
+        this._signUpFormPageLastNameInputField = By.xpath("//input[@id='s-surname']");
+        this._signUpFormPageEmailInputField = By.xpath("//input[@id='s-email']");
+        this._signUpFormPagePasswordInputField = By.xpath("//input[@id='s-password2']");
+        this._signUpFormPageCompanyInputField = By.xpath("//input[@id='s-company']");
+
+
     }
 }
 module.exports = { SignUpFormPageInvalidScenarios }
