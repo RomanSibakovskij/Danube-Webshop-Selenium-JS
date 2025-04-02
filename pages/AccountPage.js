e31c2d1c7f9ab70739b54f8ea8e088532a93896a
RomanSibakovskij <j4023958@gmail.com>
2025-04-02 16:44:05 +0300

    Add empty AccountPage sheet

---
+++ pages/AccountPage.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const { By, until} = require('selenium-webdriver');
+const BasePage = require('./utils/BasePage');
+const Logger  = require("./utils/Logger");
+const { Actions } = require('selenium-webdriver');
+const { TestDataGenerator } = require("./utils/TestDataGenerator");
+const { Key } = require('selenium-webdriver');
+
+class AccountPage extends BasePage{
+
+    constructor(driver) {
+        super(driver);
+
+        
+    }
+
+}
+module.exports = { AccountPage }
