f39c86168d54a5f8550d02a2399ed335ec1a1c5a
RomanSibakovskij <j4023958@gmail.com>
2025-04-03 04:58:24 +0300

    Add relevant web elements for AccountPageInvalidScenarios

--- pages/utils/AccountPageInvalidScenarios.js
+++ pages/utils/AccountPageInvalidScenarios.js
@@ -13,8 +13,13 @@ class AccountPageInvalidScenarios extends BasePage{
     constructor(driver) {
         super(driver);
 
+        //input elements
+        this._accountPageBillingInfoFirstNameInputField = By.xpath("//input[@id='s-name']");
+        this._accountPageBillingInfoLastNameInputField = By.xpath("//input[@id='s-surname']");
+        this._accountPageBillingInfoAddressInputField = By.xpath("//input[@id='s-address']");
+        this._accountPageBillingInfoPostCodeInputField = By.xpath("//input[@id='s-zipcode']");
+        this._accountPageBillingInfoCityInputField = By.xpath("//input[@id='s-city']");
 
-        
     }
 
 
