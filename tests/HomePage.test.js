54569b1f9494b0070f1e55fe0c2d763cc90785cd
RomanSibakovskij <j4023958@gmail.com>
2025-04-04 06:12:09 +0300

    Add additions to the test comments

--- tests/HomePage.test.js
+++ tests/HomePage.test.js
@@ -23,42 +23,42 @@ describe('Home Page Tests', () => {
 
     });
 
-    describe("Home Page Product/s Addition To Cart Test (guest)", () => {
+    describe("Home Page Product/s Addition To Cart Tests (guest)", () => {
 
-        //Test 006 -> home page product ('The Grand Grotsby') addition to cart test
+        //Test 006 -> home page product ('The Grand Grotsby') addition to cart test (as a guest)
         test("Home Page Product ('The Grand Grotsby') Addition To Cart Test (as a guest)", async function () {
             await testMethods.addHomePageGrandGrotsbyBookToCart();
         });
 
-        //Test 006a -> home page multiple products ('The Grand Grotsby', 'The Pickled Lynx') addition to cart test
+        //Test 006a -> home page multiple products ('The Grand Grotsby', 'The Pickled Lynx') addition to cart test (as a guest)
         test("Home Page Multiple Products ('The Grand Grotsby', 'The Pickled Lynx') Addition To Cart Test (as a guest)", async function () {
             await testMethods.addMultipleHomePageBooksToCart();
         });
 
     });
 
-    describe("Searched Product/s Addition To Cart Test (guest)", () => {
+    describe("Searched Product/s Addition To Cart Tests (guest)", () => {
 
-        //Test 007 -> searched product ('The Fjord of the Lies') addition to cart test
+        //Test 007 -> searched product ('The Fjord of the Lies') addition to cart test (as a guest)
         test("Searched Product ('The Fjord of the Lies') Addition To Cart Test (as a guest)", async function () {
             await testMethods.addSearchedBookFjordToCart();
         });
 
-        //Test 007a -> multiple searched products ('The Fjord of the Lies','Mostly on the Road') addition to cart test
+        //Test 007a -> multiple searched products ('The Fjord of the Lies','Mostly on the Road') addition to cart test (as a guest)
         test("Multiple Searched Products ('The Fjord of the Lies','Mostly on the Road') Addition To Cart Test (as a guest)", async function () {
             await testMethods.addMultipleSearchedBooksFjordAndRoadToCart();
         });
 
     });
 
-    describe("Single Product Category Product Addition To Cart Test (guest)", () => {
+    describe("Single Product Category Product Addition To Cart Tests (guest)", () => {
 
-        //Test 008 -> single category product page product ('The Insiders') addition to cart test
+        //Test 008 -> single category product page product ('The Insiders') addition to cart test (as a guest)
         test("Single Category Product Page Product ('The Insiders') Addition To Cart Test (as a guest)", async function () {
             await testMethods.addSingleCategoryProductToCart();
         });
 
-        //Test 008a -> single category product page multiple products ('The Insiders','Does the Sun Also Rise?') addition to cart test
+        //Test 008a -> single category product page multiple products ('The Insiders','Does the Sun Also Rise?') addition to cart test (as a guest)
         test("Single Category Product Page Multiple Products ('The Insiders', 'Does the Sun Also Rise?') Addition To Cart Test (as a guest)", async function () {
             await testMethods.addSingleCategoryMultipleProductsToCart();
         });
