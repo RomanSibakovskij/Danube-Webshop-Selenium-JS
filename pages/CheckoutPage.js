997ae1bd9cc8ef20fbd1515c13a77d1118f8d163
RomanSibakovskij <j4023958@gmail.com>
2025-04-04 06:56:11 +0300

    Add empty CheckoutPage sheet

---
+++ pages/CheckoutPage.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const { By, until} = require('selenium-webdriver');
+const BasePage = require('./utils/BasePage');
+const Logger  = require("./utils/Logger");
+const { Actions } = require('selenium-webdriver');
+const { TestDataGenerator } = require("./utils/TestDataGenerator");
+const { Key } = require('selenium-webdriver');
+
+class CheckoutPage extends BasePage {
+
+    constructor(driver) {
+        super(driver);
+
+
+        
+    }
+
+
+}
+module.exports = { CheckoutPage }
