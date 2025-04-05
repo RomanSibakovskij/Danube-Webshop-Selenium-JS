e66256d57b37c92dc69f5032d403fce64c363d71
RomanSibakovskij <j4023958@gmail.com>
2025-04-05 07:24:21 +0300

    Add empty CheckoutPageInvalidScenarios utility sheet

---
+++ pages/utils/CheckoutPageInvalidScenarios.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const { By, until} = require('selenium-webdriver');
+const BasePage = require('./BasePage');
+const Logger  = require("./Logger");
+const { TestDataGenerator } = require("./TestDataGenerator");
+
+class CheckoutPageInvalidScenarios extends BasePage{
+
+    constructor(driver) {
+        super(driver);
+
+
+
+    }
+
+
+}
+module.exports = { CheckoutPageInvalidScenarios }
